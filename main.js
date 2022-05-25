563325ea8cae7714b472b9f4fe31bf063c1adccd
DanielMalede <danielmalede@gmail.com>
2022-05-25 11:39:57 +0300

    .

--- main.js
+++ main.js
@@ -18,11 +18,21 @@
 // document.write(fruit.name)
 // document.write(fruit.color)
 // document.write(fruit.type)
-var techClass = {
-    classNum:2,
-    studentNum: 5,
-    studentsName: ["yosu","lior","david","yonatan","or"]
+// var techClass = {
+//     classNum:2,
+//     studentNum: 5,
+//     studentsName: ["yosu","lior","david","yonatan","or"]
+// }
+// for (var i = 0; i < techClass.studentsName.length; i++) {
+//     document.write(techClass.studentsName[i])
+// }
+var kindergarten = {
+    kindergarten_teacher: "Michal",
+    childernNum: 10,
+    childrenName: ["Gal","Bar","Or","Dolev","Shimon","josh","Stav","David","Ester","Micael"]
 }
-for (var i = 0; i < 5; i++) {
-    document.write(techClass.studentsName[i])
+for (var i = 0; i < kindergarten["childrenName"].length; i++) {
+    document.write(kindergarten["childrenName"][i]+"<br>")   
 }
+var userkey = prompt("enter a key")
+document.write(kindergarten[userkey])
