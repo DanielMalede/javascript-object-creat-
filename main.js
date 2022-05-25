dc727998114d3c26be56a024a1a67f057c143ab8
DanielMalede <danielmalede@gmail.com>
2022-05-25 10:18:43 +0300

    .

---
+++ main.js
@@ -0,0 +1,16 @@
+var menager = {
+    name: "daniel",
+    last_Name: "malede",
+    Age: 24,
+    Payment: 10000
+}
+var emplueid = {
+    Fall_Name: "daniel malede",
+    date_of_birth: 02/08/97,
+    email: "danielmalede@gmail.com",
+    address: "ben gavriel"
+}
+var fruit = {
+    name: "orange",
+    
+}
