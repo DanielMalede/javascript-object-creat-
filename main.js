b0b5e453111e884c92aa1abad78141c47a137945
DanielMalede <danielmalede@gmail.com>
2022-05-25 10:29:54 +0300

    .

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
-var menager = {
-    name: "daniel",
-    last_Name: "malede",
-    Age: 24,
-    Payment: 10000
+// var menager = {
+//     name: "daniel",
+//     last_Name: "malede",
+//     Age: 24,
+//     Payment: 10000
+// };
+// var emplueid = {
+//     Fall_Name: "daniel malede",
+//     date_of_birth: 02/08/97,
+//     email: "danielmalede@gmail.com",
+//     address: "ben gavriel"
+// }
+// var fruit = {
+//     name: "orange",
+//     color: "orange",
+//     type: "vagtable"
+// }
+// document.write(fruit.name)
+// document.write(fruit.color)
+// document.write(fruit.type)
+var techClass = {
+    classNum:2,
+    studentNum: 5,
+    studentsName: ["yosu","lior","david","yonatan","or"]
 }
-var emplueid = {
-    Fall_Name: "daniel malede",
-    date_of_birth: 02/08/97,
-    email: "danielmalede@gmail.com",
-    address: "ben gavriel"
-}
-var fruit = {
-    name: "orange",
-    
+for (var i = 0; i < 5; i++) {
+    document.write(techClass.studentsName[i])
 }
